84a82cc55eb502e6ac0d38911c7a657e1b0ddd53
Jorge Lepasana <jorgelepasana8@gmail.com>
2025-04-13 23:56:43 +0800

    Update script.js
    updated pic

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ document.addEventListener('DOMContentLoaded', function() {
     // Slideshow data - replace with your own images and captions
     const slides = [
         { 
-            imageUrl: src="images/5.jpg",
+            imageUrl: src='/images/5.jpg',
             caption: 'Celebrating you!'
         },
         { 
-            imageUrl: src="images/2.jpg",
+            imageUrl: src='/images/2.jpg',
             caption: 'Make a wish!'
         },
         { 
-            imageUrl: src="images/6.jpg",
+            imageUrl: src='/images/6.jpg',
             caption: 'soft but strong'
         },
         { 
-            imageUrl: src="images/3.jpg",
+            imageUrl: src='/images/5.jpg',
             caption: 'Enjoy your day!',
         },
         { 
-            imageUrl: src="images/4.jpg",
+            imageUrl: src='/images/4.jpg',
             caption: 'One of one.'
         },
         { 
-            imageUrl: src="images/1.jpg",
+            imageUrl: src='/images/1.jpg',
             caption: 'Happy moments!'
         }
     ];
